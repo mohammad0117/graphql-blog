07e3f2b2e8c07312b4f085b1dc65286dc7bec7eb
mohammad molavi <mohamadmolavi98@gmail.com>
2024-05-06 08:59:29 +0430

    create header component and develop that

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ThemeProvider } from "@emotion/react";
 
-import "./index.css";
+import "./styles/index.css";
+import "./styles/fonts.css";
 import App from "./App";
+import theme from "./mui/theme";
 
 const client = new ApolloClient({
   uri: "https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clvomu0sm0m7007wht67xfh8s/master",
   cache: new InMemoryCache(),
 });
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </ApolloProvider>
 );
